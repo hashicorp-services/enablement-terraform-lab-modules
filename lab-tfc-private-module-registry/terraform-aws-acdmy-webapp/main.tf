1a737e86aeb0d61df04890aedbe2cb8e2660ea4f
amarrone-hashicorp <86081038+amarrone-hashicorp@users.noreply.github.com>
2023-05-09 13:40:52 -0700

    fix to AWS S3 bucket ACLs

--- lab-tfc-private-module-registry/terraform-aws-acdmy-webapp/main.tf
+++ lab-tfc-private-module-registry/terraform-aws-acdmy-webapp/main.tf
@@ -16,32 +16,30 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
+  depends_on = [
+	aws_s3_bucket_public_access_block.bucket,
+	aws_s3_bucket_ownership_controls.bucket,
+  ]
 
-  acl = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.bucket.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
-            ]
-        }
-    ]
-}
-EOF
+  acl    = "public-read"
 }
 
 resource "aws_s3_object" "webapp" {
