7164a3b3a2373403ec6937d6fa76919f57abc888
Allison Broski <allison.broski@hashicorp.com>
2024-06-28 12:51:28 -0500

    test: startup script change

--- lab-tfc-hashicat-gcp/main.tf
+++ lab-tfc-hashicat-gcp/main.tf
@@ -66,7 +66,8 @@ resource "google_compute_instance" "hashicat" {
     ssh-keys = "ubuntu:${chomp(tls_private_key.ssh-key.public_key_openssh)} terraform"
   }
 
-  metadata_startup_script = "sudo echo hi > /test.txt"
+  metadata_startup_script = '#! /bin/bash
+  sudo echo hi > /test.txt'
 
   tags = ["http-server"]
 
