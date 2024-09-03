513b002edd301a7bb11f88ba1281683ebea3b5ff
flamacue <adam.sumner@hashicorp.com>
2024-09-03 11:42:22 -0500

    fix: use static ip allocation

--- hashicat-azure/main.tf
+++ hashicat-azure/main.tf
@@ -105,7 +105,7 @@ resource "azurerm_public_ip" "catapp-pip" {
   name                = "${var.user}-ip"
   location            = azurerm_resource_group.myresourcegroup.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   domain_name_label   = "${var.user}-meow"
 }
 
